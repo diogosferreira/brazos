fcea23a78ae8d7d9bce1698da794f189b19798f8
diogosferreira <diogosferreira@users.noreply.github.com>
2025-07-03 18:14:31 +0100

    section_sectors

--- public/code.js
+++ public/code.js
@@ -1,7 +1,7 @@
 import { backgroundBlue } from './animations/blue-background.js';
 import { imageClip } from './animations/image-clip-reveal.js';
 import { lineGrow } from './animations/line-grow.js';
-import { sectors } from './animations/sectors-reveal-home.js';
+
 import { tabsEnter } from './animations/tabs-enter-from-right.js';
 import { benefitsTabs } from './benefits/benefits-tabs.js';
 import { formButtonSubmit } from './forms/button-form-submit.js';
@@ -44,7 +44,7 @@ import { filters } from './filters.js';
 backgroundBlue();
 imageClip();
 lineGrow();
-sectors();
+
 tabsEnter();
 benefitsTabs();
 formButtonSubmit();
